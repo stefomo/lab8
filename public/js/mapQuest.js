dde3f3e59e30c7b1bc993c27f77fbbcd9161b6fb
Stefanie Mendoza <svm005@ucsd.edu>
2020-02-27 13:25:44 -0800

    js

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,15 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'FwzD5MFPa8GMf02sCitGqEurGAZxS8om';
+
+	// 'map' refers to a <div> element with the ID map
+  	var map = L.mapquest.map('map', {
+  	center: [32.8805, -117.2394],
+  	layers: L.mapquest.tileLayer('map'),
+  	zoom: 12,
+  	zoomControl: false 
+	});
+
+	//Add a marker to the map
+	L.marker([32.8805, -117.2394]).addTo(map);
 }
